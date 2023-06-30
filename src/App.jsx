242130797934c74b26568559bc0c0ec8a2f06244
Shelton Simbi <shltnsimbi@gmail.com>
2023-06-30 14:10:10 +0200

    created chatFeed component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css";
+import ChatFeed from "./components/ChatFeed";
 import { ChatEngine } from "react-chat-engine";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="
+  23d9a472-590d-49f1-aa78-8d7240a566b7"
+      userName="Mr Classic"
+      userSecret="123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
 };
 
 export default App;
